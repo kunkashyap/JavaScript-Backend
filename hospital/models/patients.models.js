17bb855dd735180505c791a4d20be7f0a1ad412a
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-08-04 00:37:52 +0530

    schemas for doctors,hospitals,patients

--- hospital/models/patients.models.js
+++ hospital/models/patients.models.js
@@ -1,3 +1,37 @@
 import mongoose from 'mongoose';
 
-const patientSchema = new mongoose.Schema({}, {timestamps: true})
+const patientSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    diagnosedWith : {
+        type : String,
+        required: true
+    },
+    address : {
+        type : String,
+        required: true
+    },
+    age : {
+        type : Number,
+        required : true
+    },
+    bloodGroup : {
+        enum : ["AB+","AB-","A+","A-","B+","B-","O+","O-"],
+        required : true
+    },
+    gender : {
+        type : String,
+        enum : ["Male","Female","Other"],
+        required : true
+    },
+    admittedIn : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Hospital"
+    }
+
+}, {timestamps: true})
+
+
+export const Patient = mongoose.model('Patient',patientSchema)
