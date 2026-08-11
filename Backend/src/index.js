855203bcecd3318528b6c15c75541cbc447b834e
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-08-11 23:14:28 +0530

    connecting database

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,34 @@
-console.log('Happy developing ✨')
+import mongoose from 'mongoose'
+import {DB_NAME} from './constants.js'
+import express from 'express'
+
+const app = express()
+//While connecting database always use async / try-catch
+
+
+
+
+
+
+
+
+
+
+
+
+//iffe
+;(async ()=> {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=> {
+            console.log("ERROR:",error)
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`APP IS LISTENING ON ${process.env.PORT}`)
+        })
+
+    } catch (error) {
+    console.log("ERROR", error)
+    }
+}) ()
