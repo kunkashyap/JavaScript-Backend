1972f3a41c09ba1f115418903ac9e08c4b843de2
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-08-14 18:12:04 +0530

    handling api errors

---
+++ Backend/src/utils/APIErrors.js
@@ -0,0 +1,26 @@
+//Handling API Errors
+
+class APIErrors extends Error {
+    constructor (
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+
+    ){
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = this.errors
+
+        if (stack) {
+            this.stack = stack;
+
+        }else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+
+    }
+}
